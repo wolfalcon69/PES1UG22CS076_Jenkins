e9b4c26c824347380cd0afccaf9edd83efba189d
Amogh Patil <amogh.patil.2004@gmail.com>
2025-03-20 20:13:19 +0530

    commit changes

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,10 +1,11 @@
-//File: hello.cpp
+// File: hello.cpp
 #include <iostream>
 using namespace std;
 
-int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
+int main(void)
+{
+  cout << "Hello World\n";
+  cout << "Hello Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
